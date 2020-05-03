6e99a6d135883ddb211a505857c7ff843d67bdea
vehx <delta222@gmail.com>
2020-05-03 20:09:55 +0200

    added fetch to api yay, still wip :)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     // start values, change to speed up testing :)
     const startMoney = 100;
     const startIncome = 0;
+    const apiUrl = "http://localhost:5000/player/2";
 
     const [money, setMoney] = React.useState(100);
     const [income, setIncome] = React.useState(1);
@@ -55,7 +56,57 @@ function App() {
         setMoney(startMoney);
         setIncome(startIncome);
         setIsFirstTime(false);
+
+        // test api
+        GetInfo();
     };
+
+    // what incoming json will look like in get request
+    // {
+    //     "data": {
+    //         "id": 2,
+    //         "name": "Tester2",
+    //         "money": 7309,
+    //         "income": 1,
+    //         "buildings": [
+    //             {
+    //                 "id": 0,
+    //                 "name": "Farm",
+    //                 "owned": 0,
+    //                 "cost": 100,
+    //                 "incomeIncrease": 1,
+    //                 "costIncrease": 1.2,
+    //                 "upgrade": 500
+    //             }
+    //         ]
+    //     },
+    //     "success": true,
+    //     "message": null
+    // }
+
+    const GetInfo = () => {
+        fetch(apiUrl, {
+            method: "get",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            redirect: "follow",
+            // referrerPolicy: "no-referrer",
+        })
+            .then((r) => r.json())
+            .then((r) => {
+                // let player = r.data;
+                console.log(r);
+            });
+    };
+
+    // template for put request when buying building
+    // {
+    //     "id": 2,
+    //     "buildingId": 0
+    // }
 
     // template for setBuildingElements to use when rendering buildings
     const buildingsRender = buildings.map((building, key) => (
@@ -104,7 +155,11 @@ function App() {
     return (
         <div className="App">
             <Title>Idle Farms</Title>
-            <GameInfo currentMoney={money} currentIncome={income} />
+            <GameInfo
+                playerName={"MakeVariable"}
+                currentMoney={money}
+                currentIncome={income}
+            />
             {buildingElements}
         </div>
     );
